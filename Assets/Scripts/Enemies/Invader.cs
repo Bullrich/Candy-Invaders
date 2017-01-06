72d924593c9717e79d4b9dcd8c39e14efa413e8c
Javyer <javierbullrich@gmail.com>
2017-01-06 16:20:51 -0300

    #2 The grid system can create grids based on a width and height given. The distance between the enemies can also be definied. #1 The basic enemy has been created. For the moment it only change color and get some settings.

---
+++ Assets/Scripts/Enemies/Invader.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//By @JavierBullrich
+
+namespace Game.Enemies {
+	public class Invader : MonoBehaviour {
+        public enum InvaderColor
+        {
+            Yellow,
+            Red,
+            Blue,
+            Green
+        }
+        public InvaderColor shipColor;
+        SpriteRenderer spr;
+
+        private void Awake()
+        {
+            spr = GetComponent<SpriteRenderer>();
+            spr.color = getColor();
+        }
+
+        public float getSpriteWidth()
+        {
+            if(spr == null)
+                GetComponent<SpriteRenderer>();
+            return spr.sprite.rect.width;
+        }
+
+        Color getColor()
+        {
+            Color color = Color.white;
+            switch (shipColor)
+            {
+                case InvaderColor.Yellow:
+                    color = Color.yellow;
+                    break;
+                case InvaderColor.Red:
+                    color = Color.red;
+                    break;
+                case InvaderColor.Blue:
+                    color = Color.blue;
+                    break;
+                case InvaderColor.Green:
+                    color = Color.green;
+                    break;
+                default:
+                    break;
+            }
+            return color;
+        }
+
+		void Start () {
+            //print(getSpriteWidth());
+		}
+	}
+}
---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Enemies;
+//By @JavierBullrich
+
+namespace Game.Grid {
+	public class GridSystem : MonoBehaviour {
+        public Invader invader;
+        public int height, width;
+        [Range(0.7f, 2)]
+        public float distanceBetween = 0.9f;
+
+		void Start () {
+            PopulateGrid();
+		}
+
+        private void PopulateGrid()
+        {
+            //float distance = (invader.getSpriteWidth() / 50);
+            Vector2 spawnPos = transform.position;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    GameObject enemy = Instantiate(invader.gameObject);
+                    enemy.transform.position = spawnPos + new Vector2(distanceBetween * j, 0);
+                }
+                spawnPos.y -= distanceBetween;
+            }
+            
+        }
+	}
+}
